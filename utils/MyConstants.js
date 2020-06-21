c097d4a5c72d3bd0b93f9d2ac608630dba932c2b
Trần Kiến Tường <55739577+trankientuong@users.noreply.github.com>
2020-06-21 21:15:07 +0700

    Up project
    up

---
+++ utils/MyConstants.js
@@ -0,0 +1,10 @@
+var MyConstants = {
+  DB_SERVER: 'cluster0-liy2s.mongodb.net',
+  DB_USER: 'tuong1106',
+  DB_PASS: '14243456',
+  DB_DATABASE: 'shoppingonline',
+  HOSTNAME: 'localhost:3000',
+  EMAIL_USER: 'tuong.187pm20607@vanlanguni.vn',
+  EMAIL_PASS: 'VLU187pm20607',
+};
+module.exports = MyConstants;
